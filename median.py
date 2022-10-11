00926742a2a51713e174186c2805f785d27cb91d
Pavan-Rana <psrbr157@gmail.com>
2022-10-11 15:37:04 +0100

    attempt

--- median.py
+++ median.py
@@ -11,4 +11,7 @@ while True:
         break
 numbers.sort()
 middle =  len(numbers) // 2
-print(numbers[middle]+numbers[~middle]/2)
+if(len[numbers]%2==1):
+    print(numbers[middle])
+else:
+    print(numbers[middle]+numbers[middle+1]/2)
